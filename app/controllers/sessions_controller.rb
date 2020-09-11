1f909df9354543e7152a9fcb8f74e3231c9d971a
Abel Kalum <chumbabel@gmail.com>
2020-09-11 17:43:37 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
-    def new
-      
+  def new
+  end
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to :login
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
     end
- 
-    def create
-        session[:username] = params[:username]
-        redirect_to '/'
+  end
+
+  def destroy
+    if !session[:name].nil?
+      session.delete :name
     end
+    redirect_to '/'
+  end
 end
